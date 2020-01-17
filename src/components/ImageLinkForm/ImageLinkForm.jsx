e025451b6fcacbae417e11535b018146f9b81e4b
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-17 10:58:54 -0600

    Implement ImageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './ImageLinkForm.css';
 const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3">
-        {'This magic face will detect faces in your picture. Get a try'}
+        {'This magic face will detect faces in your picture. Get a try!!!'}
       </p>
       <div className="center">
-        <div className="pa4 br3 shadow-5">
+        <div className="form center pa4 br3 shadow-5">
           <input className="f4 pa2 w-70 center" type="text" />
           <button className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple">
             {' '}
