fc240932837baa3db8509062ba32e6fdcb9ca2b5
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-17 12:35:52 -0600

    using Clarifai api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Clarifai from 'clarifai';
+
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -6,6 +8,10 @@ import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'd924602a97ab43fd91585703f5cdd881',
+});
 
 const particlesOptions = {
   particles: {
@@ -33,18 +39,52 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
 
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecogition />*/}
-    </div>
-  );
+  handleChange = event => {
+    console.log(event.target.value);
+  };
+
+  handleSubmit = () => {
+    console.log('click');
+    app.models
+      .predict(
+        'a403429f2ddf4b49b307e318f00e528b',
+        'https://samples.clarifai.com/face-det.jpg'
+      )
+      .then(
+        function(response) {
+          // do something with response
+          console.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        {/*<FaceRecogition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
-const ImageLinkForm = () => {
+
+const ImageLinkForm = ({ handleChange, handleSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -8,8 +9,15 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
-          <button className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={handleChange}
+          />
+          <button
+            className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple"
+            onClick={handleSubmit}
+          >
             {' '}
             Detect
           </button>
